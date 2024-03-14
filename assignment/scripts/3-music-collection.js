d03142bbb4d2cd6b2b9526ab48b5b7cbf0a6aaaf
Susie Douang <soutsada.d@gmail.com>
2024-03-14 15:36:04 -0500

    do initial commit

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,10 +1,24 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
 // Safe Zone -- Write code below this line
 
+// Create an empty array named myCollection.
+let myCollection = [];
 
+// Create a function named addToCollection. 
 
+// Use and Test the addToCollection function
 
+// Create a function named showCollection.
 
+// Test the showCollection function.
+
+// Add a function named findByArtist
+
+// Test the findByArtist function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection
+
+// Create a function called search that will allow for searching by artist and yearPublished
+
+// Add an array of tracks to each of your album objects.
 
 
 
@@ -14,12 +28,16 @@ console.log('***** Music Collection *****')
 // (It's used for automated testing.)
 try {
   module.exports = {
-    myCollection: typeof myCollection !== 'undefined' ? myCollection : undefined,
-    addToCollection: typeof addToCollection !== 'undefined' ? addToCollection : undefined,
-    showCollection: typeof showCollection !== 'undefined' ? showCollection : undefined,
-    findByArtist: typeof findByArtist !== 'undefined' ? findByArtist : undefined,
+    myCollection:
+      typeof myCollection !== 'undefined' ? myCollection : undefined,
+    addToCollection:
+      typeof addToCollection !== 'undefined' ? addToCollection : undefined,
+    showCollection:
+      typeof showCollection !== 'undefined' ? showCollection : undefined,
+    findByArtist:
+      typeof findByArtist !== 'undefined' ? findByArtist : undefined,
     search: typeof search !== 'undefined' ? search : undefined,
-  }
+  };
 } catch (e) {
   // Do nothing
 }
